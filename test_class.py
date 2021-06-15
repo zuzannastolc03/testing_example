4c40a46cf56d9ceb4cd7a875ee4c2a35be1f19c9
zuzannastolc03 <zuzanna.stolc03@gmail.com>
2021-06-15 15:52:30 +0200

    first commit

---
+++ test_class.py
@@ -0,0 +1,6 @@
+class TestClass:
+    name = "my name"
+    
+    def test_one(self):
+        assert 'h' in self.name
+
---
+++ test_sample.py
@@ -0,0 +1,6 @@
+def increment(x):
+    return x + 1
+
+def test_answer():
+    assert increment(3) == 5
+
